8f9c93839bbea0dcfe10b74e30c2022680b335fc
Dario Alberto Delgado Portillo <89899431+daridel99@users.noreply.github.com>
2022-10-06 14:38:33 -0500

    Update redes.js

--- src/database/redes.js
+++ src/database/redes.js
@@ -2,7 +2,7 @@ export const redes = [
     {
       id: 1,
       titulo: "wpp",
-      url: "https://pps.whatsapp.net/v/t61.24694-24/295143036_151670217465767_8500922905199564714_n.jpg?stp=dst-jpg_s96x96&ccb=11-4&oh=01_AVzfD6CM7JYJOmbG0h0xA0lbkmt3P8tBMZHnMNdOvM7gIA&oe=633E793C",
+      url: "https://pps.whatsapp.net/v/t61.24694-24/295143036_151670217465767_8500922905199564714_n.jpg?ccb=11-4&oh=01_AVzBTeXzcpyNZL1Q1lwacvqPNABPiPrMT9ThuW_1Uvsd_g&oe=63500D3C",
       url_preview: "https://api.whatsapp.com/send?phone=573133589307",
     },
     {
