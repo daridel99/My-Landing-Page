397479ad9403ce864ba694821f24ff3da5c7569c
daridel99 <darialber@gmail.com>
2023-04-02 21:28:10 -0500

    se pone imagen de la HDV y se puede descargar

--- src/pages/HDV.jsx
+++ src/pages/HDV.jsx
@@ -6,7 +6,9 @@ import pdfFile from '/hojadevida_2.pdf';
 export const HDV = () => {
   return (
 <>
-    <h1 className='font-bold text-2xl'>Hoja de Vida</h1>
+    <h1 className='font-bold text-2xl'>
+    <a class="btn btn-download" href="/public/hojadevida_2.pdf" download="hojadevida_2.pdf">click DOWNLOAD Hoja de Vida</a>
+      </h1> 
     <div class="scrolling-wrapper-y">
     <img src="/hojadevida_2_page1.jpg" alt="HDV" width="75%" height="75%"/>
     </div>
