eea064fdcc1c1f6225d131899e9d710fa266930d
Dario Alberto Delgado Portillo <89899431+daridel99@users.noreply.github.com>
2023-04-02 21:31:39 -0500

    Update HDV.jsx

--- src/pages/HDV.jsx
+++ src/pages/HDV.jsx
@@ -7,7 +7,7 @@ export const HDV = () => {
   return (
 <>
     <h1 className='font-bold text-2xl'>
-    <a class="btn btn-download" href="/public/hojadevida_2.pdf" download="hojadevida_2.pdf">click DOWNLOAD Hoja de Vida</a>
+    <a class="btn btn-download" href="/public/hojadevida_2.pdf" download="/public/hojadevida_2.pdf">click DOWNLOAD Hoja de Vida</a>
       </h1> 
     <div class="scrolling-wrapper-y">
     <img src="/hojadevida_2_page1.jpg" alt="HDV" width="75%" height="75%"/>
