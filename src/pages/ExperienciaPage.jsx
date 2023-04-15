76c3ee9ccd3229d48c2d7b3bd3b004caccab8cc8
Dario Alberto Delgado Portillo <89899431+daridel99@users.noreply.github.com>
2023-04-15 17:16:53 -0500

    Update ExperienciaPage.jsx

--- src/pages/ExperienciaPage.jsx
+++ src/pages/ExperienciaPage.jsx
@@ -11,12 +11,13 @@ export const ExperienciaPage = () => {
       Experiencia centrada en dar solución a problemáticas  
       centradas en control, visualización de datos <br/> y manejo de estos, 
       además de Dash Board que permiten tener una ejecución y recolección 
-      <br/> de datos en tiempo real, también entornos 3D con el propósito de inmersión, en el ambito academico.
-      <br/><br/>
+     <br/> de datos en tiempo real, también entornos 3D con el propósito de inmersión, en el ambito academico.
+     <br/><br/>
       Mi experiencia laboral radica en desarrollo web, logística en festivales 
       de robótica; además de la atención <br/> al cliente y diseño de modelos 3D para 
       prototipado e impresión.
     <br/><br/>
+      Actualmente me encuentro realizando las practicas laborales en el Banco Falabella SA.
   {/* Y en conjunto contigo podré crear e iniciar mi camino laboral. */}
 </h3>
       </div>
